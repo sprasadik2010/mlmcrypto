015e21c117c69b8b1d1851bbf98686b4f10bc6ac
REBOSOLUTIONS <siva.prasad@rebosolution.com>
2025-03-28 19:11:54 +0530

    404 Err Fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/CryptoPayment/', // 🔹 Add this line (your repo name)
+  base: '/', // 🔹 Add this line (your repo name)
   plugins: [
     tailwindcss(),
     react()
