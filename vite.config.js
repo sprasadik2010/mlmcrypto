03637787e9a24593df24e8104aaa837e24f1a7d4
REBOSOLUTIONS <siva.prasad@rebosolution.com>
2025-03-28 19:05:24 +0530

    Initial commit for MLM Crypto project

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/CryptoPayment/', // 🔹 Add this line (your repo name)
+  plugins: [
+    tailwindcss(),
+    react()
+  ],
+})
